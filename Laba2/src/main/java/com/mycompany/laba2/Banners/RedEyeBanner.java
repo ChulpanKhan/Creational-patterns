aa322ef7c94a6dd2165072b4705b339b6de7d801
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-03-28 12:30:52 +0300

    почти все классы есть

---
+++ Laba2/src/main/java/com/mycompany/laba2/Banners/RedEyeBanner.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.laba2.Banners;
+
+/**
+ *
+ * @author MyHuawei
+ */
+public class RedEyeBanner implements Banner {
+    
+}
---
+++ Laba2/src/main/java/com/mycompany/laba2/Ork.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.laba2;
+
+/**
+ *
+ * @author MyHuawei
+ */
+public class Ork {
+    private String name;
+    private String weapon;
+    private String armor;
+    private String banner;
+    
+}
