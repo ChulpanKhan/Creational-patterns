637b993f8cd4f7fe213bd48decb29f1addcd6eaa
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-03-31 00:58:49 +0300

    Update Ork.java

--- Laba2/src/main/java/com/mycompany/laba2/Ork.java
+++ Laba2/src/main/java/com/mycompany/laba2/Ork.java
@@ -2,12 +2,15 @@
 package com.mycompany.laba2;
 
 import com.github.javafaker.Faker;
+import com.mycompany.laba2.Armors.Armor;
+import com.mycompany.laba2.Banners.Banner;
+import com.mycompany.laba2.Weapons.Weapon;
 
 public class Ork {
     private String name;
-    private String weapon;
-    private String armor;
-    private String banner;
+    private Weapon weapon;
+    private Armor armor;
+    private Banner banner;
     private int strength;
     private int agility;
     private int intelligence;
@@ -15,7 +18,7 @@ public class Ork {
     private String tribe;
     private String role;
     
-    public Ork(String name, String weapon, String armor, String banner, 
+    public Ork(String name, Weapon weapon, Armor armor, Banner banner, 
             int strength, int agility, int intelligence, int health,
             String tribe, String role) {
         //this.name = new Faker().lordOfTheRings().character();
@@ -30,4 +33,15 @@ public class Ork {
         this.tribe = tribe;
         this.role = role;
     }
+    
+    public String getName() { return name; }
+    public Weapon getWeapon() { return weapon; }
+    public Armor getArmor() { return armor; }
+    public Banner getBanner() { return banner; }
+    public int getStrength() { return strength; }
+    public int getAgility() { return agility; }
+    public int getIntelligence() { return intelligence; }
+    public int getHealth() { return health; }
+    public String getTribe() { return tribe; }
+    public String getRole() { return role; }
 }
